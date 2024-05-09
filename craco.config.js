2363b378835cbfab514a2e146d34b5f19cb64830
Mario Zelger <mario.zelger@hotmail.com>
2024-05-09 14:42:27 +0200

    chore(deps): removed unused dependencies

--- craco.config.js
+++ craco.config.js
@@ -1,15 +1,5 @@
 const CracoLessPlugin = require("craco-less");
 
-const emotionBabelPreset = require("@emotion/babel-preset-css-prop").default(
-  undefined,
-  {} // emotion preset options
-);
-
 module.exports = {
-  plugins: [{ plugin: CracoLessPlugin }],
-  babel: {
-    plugins: [
-      ...emotionBabelPreset.plugins
-    ]
-  }
+    plugins: [{plugin: CracoLessPlugin}]
 };
