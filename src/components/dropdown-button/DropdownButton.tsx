57bc2959ea02c93ff52bf3dbb2f540698e43a442
Mario Zelger <mario.zelger@hotmail.com>
2024-06-09 16:52:23 +0200

    style: added dropdown button component

---
+++ src/components/dropdown-button/DropdownButton.tsx
@@ -0,0 +1,69 @@
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
+import './dropdown-button.less';
+
+type Props = {
+    title?: string;
+    buttonContent?: ReactNode;
+    children: ReactNode;
+}
+
+interface OnClickCallback {
+    onClick?: () => void;
+}
+
+const DropdownButton = ({ title, buttonContent, children }: Props) => {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+            setShowDropdown(false);
+        }
+    };
+
+    const handleOptionClick = (optionCallback?: () => void) => {
+        setShowDropdown(false);
+        if (optionCallback) {
+            optionCallback();
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div className="dropdown-container" ref={dropdownRef}>
+            <button className="btn-primary dropdown-button" onClick={toggleDropdown}>
+                {!!title ? (
+                    title
+                ) : (!!buttonContent ? (
+                        buttonContent
+                    ) : ""
+                )}
+            </button>
+            {showDropdown && (
+                <div className="dropdown-content">
+                    {React.Children.map(children, (child) => {
+                            if (React.isValidElement<OnClickCallback>(child)) {
+                                return React.cloneElement(child, {
+                                    onClick: () => handleOptionClick(child.props.onClick),
+                                });
+                            }
+                            return child;
+                        }
+                    )}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default DropdownButton;
