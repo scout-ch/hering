69f64c8e3bc456246ba2b9d570956668741d2e44
Mario Zelger <15825982+mario-zelger@users.noreply.github.com>
2025-11-12 20:28:37 +0100

    perf: load search page content from directly from translations instead of parsing JSON

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,13 +1,5 @@
-import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Markdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-import { LinkComponent } from '../../helper/MarkdownComponents'
-import { i18n } from '../../i18n'
 import SearchForm from './components/SearchForm'
-import searchPageDE from "../../data/search-page/de.json"
-import searchPageFR from "../../data/search-page/fr.json"
-import searchPageIT from "../../data/search-page/it.json"
 import { faSearch } from "@fortawesome/free-solid-svg-icons"
 import './search.less'
 import { type HApiSection } from "../../apis/hering-api";
@@ -24,37 +16,17 @@ export type SearchPageT = {
 
 function SearchPage(props: Props) {
 
-    const lang = i18n.language
-    const [searchPage, setSearchPage] = useState<SearchPageT>();
+function SearchPage() {
 
-    useEffect(() => {
-        switch (i18n.language) {
-            case 'de':
-                return setSearchPage(searchPageDE)
-            case 'fr':
-                return setSearchPage(searchPageFR)
-            case 'it':
-                return setSearchPage(searchPageIT)
-            default:
-                return setSearchPage(searchPageDE)
-        }
-    }, [lang])
-
-    if (!searchPage) {
-        return null
-    }
+    const { t } = useTranslation()
 
     return <>
-        <DocumentTitle title={searchPage.title}/>
+        <DocumentTitle title={t('searchPage.title')}/>
         <div className='content-main'>
             <div className='search'>
                 <h1>
-                    <FontAwesomeIcon icon={faSearch}/> {searchPage.title}
+                    <FontAwesomeIcon icon={faSearch}/> {t('searchPage.title')}
                 </h1>
-                <Markdown remarkPlugins={[remarkGfm]}
-                          components={LinkComponent}>
-                    {searchPage.content}
-                </Markdown>
 
                 <SearchForm sections={props.sections}/>
             </div>
